6c328dcc707ce4151777760ec674ed920252e596
vivi-barba <vitoriabbarbosa31@gmail.com>
2023-03-12 22:25:20 -0300

    feat: estudando objeto

---
+++ object/script.js
@@ -0,0 +1,17 @@
+// Animal properties and method encapsulation
+const Animal = {
+  type: "Invertebrates", // Default value of properties
+  displayType() {
+    // Method which will display type of Animal
+    console.log(this.type);
+  },
+};
+
+// Create new animal type called animal1
+const animal1 = Object.create(Animal);
+animal1.displayType(); // Logs: Invertebrates
+
+// Create new animal type called fish
+const fish = Object.create(Animal);
+fish.type = "Fishes";
+fish.displayType(); // Logs: Fishes
