6433a6648cf3ce61f4b5943f1db138513355b3f1
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-03-23 01:05:36 -0300

    add exercicio

--- conditinal/exercicios/imc.js
+++ conditinal/exercicios/imc.js
@@ -13,7 +13,7 @@ IMC em adultos Condição:
 - Acima de 40 Obsesidade Grave;
  */
 
-var peso = 63;
+var peso = 130;
 var altura = 1.72;
 
 var imc = peso / (altura * altura);
@@ -21,4 +21,12 @@ console.log(imc.toFixed(2))
 
 if(imc < 18.5){
     console.log("Você esta abaixo do peso");
-} else if(imc === 18.5 || 25)
+} else if(imc >= 18.5 && imc < 25){
+    console.log("Peso normal");
+} else if (imc >= 25 && imc < 30 ){
+    console.log("Acima do peso");
+} else if (imc >= 30 && imc < 40){
+    console.log("Obeso");
+} else {
+    console.log("obesidade grave");
+}
