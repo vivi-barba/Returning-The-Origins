582684bc184475109f5914ee69de6f114623caf6
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-04-10 23:27:44 -0300

    add array

---
+++ array/index.js
@@ -0,0 +1,21 @@
+const aluno = ["Carlos", "Jessica", "José"];
+
+console.log(aluno[2]);
+//Formas de adicionar dados ao array
+aluno.push("Fabiana");
+
+console.log(aluno);
+
+aluno[4] = "Vinicius";
+
+console.log(aluno);
+
+//Formas de exclui dados
+
+console.log(aluno.pop()); // Tira o ultimo item do array
+
+console.log(aluno.shift());// Tira o primeiro item do array
+
+delete aluno[3];
+
+
