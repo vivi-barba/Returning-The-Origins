6ccd851a606b7622b945819b880827e2559604f4
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-04-12 13:27:37 -0300

    feat: add array

---
+++ array/for.js
@@ -0,0 +1,4 @@
+for (let index = 1; index < 11; index++) {
+   console.log(index)
+    
+}
---
+++ array/script.js
@@ -0,0 +1,16 @@
+const notas = [];
+
+notas.push(7);
+notas.push(6);
+notas.push(8);
+notas.push(8);
+notas.push(9);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+
+for (let index = 0; index < array.length; index++) {
+  
+    
+}
+
+console.log(soma / 5);
